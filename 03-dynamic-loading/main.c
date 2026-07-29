aa0ef064a6337df80cbafe4e2dcab53ee2c9c5c1
mochou-p <66534926+mochou-p@users.noreply.github.com>
2026-07-30 08:10:46 +0900

    dynamic loading

---
+++ 03-dynamic-loading/main.c
@@ -0,0 +1,41 @@
+/* mochou-p/dependen.c/03-dynamic-loading/main.c */
+
+#include <dlfcn.h>
+#include <stdio.h>
+
+int main(void) {
+    void *handle = dlopen("libother.so", RTLD_NOW);
+    if (handle == NULL) {
+        fprintf(stderr, "dlopen(\"libother.so\", RTLD_NOW) failed: %s\n", dlerror());
+        return 1;
+    }
+
+    dlerror(); /* clear errors as a good citizen */
+    void (*print_message)(void) = dlsym(handle, "print_message");
+
+    char *error = dlerror();
+    if (error != NULL) {
+        fprintf(stderr, "dlsym(handle, \"print_message\") failed: %s\n", error);
+        if (dlclose(handle) != 0) {
+            fprintf(stderr, "dlclose(handle) failed: %s\n", dlerror());
+        }
+        return 1;
+    }
+
+    if (print_message == NULL) {
+        fprintf(stderr, "symbol \"print_message\" loaded, but points to NULL\n");
+        if (dlclose(handle) != 0) {
+            fprintf(stderr, "dlclose(handle) failed: %s\n", dlerror());
+        }
+        return 1;
+    }
+
+    print_message();
+
+    if (dlclose(handle) != 0) {
+        fprintf(stderr, "dlclose(handle) failed: %s\n", dlerror());
+        return 1;
+    }
+
+    return 0;
+}
---
+++ 03-dynamic-loading/other.c
@@ -0,0 +1,7 @@
+/* mochou-p/dependen.c/03-dynamic-loading/other.c */
+
+#include <stdio.h>
+
+void print_message(void) {
+    puts("hello world from a dynamically loaded .so file");
+}
